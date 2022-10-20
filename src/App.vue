97b333362ad8b2cc7a8a7a59792c4b35c7e6686d
Parvez M Robin <parvezmrobin@gmail.com>
2022-10-20 00:40:25 -0300

    Added contact page

--- src/App.vue
+++ src/App.vue
@@ -13,21 +13,23 @@
         <Certifications />
         <Achievements />
         <Activities />
+        <Contact />
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import Achievements from "./components/Achievements.vue";
 import Activities from "./components/Activities.vue";
+import Certifications from "./components/Certifications.vue";
+import Contact from "./components/Contact.vue";
 import Education from "./components/Education.vue";
 import Experience from "./components/Experience.vue";
 import Home from "./components/Home.vue";
 import Navbar from "./components/Navbar.vue";
 import Projects from "./components/Projects.vue";
 import Skills from "./components/Skills.vue";
-import Certifications from "./components/Certifications.vue";
-import Achievements from "./components/Achievements.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/Contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="contact" class="pt-5 row justify-content-center page">
+    <div class="central-box flex-column">
+      <Heading class="pb-4">Want to know more?</Heading>
+      <h4 class="fw-light">
+        Why don’t send an
+        <a
+          href="mailto:parvezmrobin@gmail.com"
+          class="text-pink text-decoration-none"
+          >email</a
+        >
+        and let’s talk.
+        <img src="/thinking.gif" alt="I Wonder!" style="height: 40px" />
+      </h4>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Heading from "./Heading.vue";
+</script>
+
+<style lang="css">
+#contact {
+  --central-box-margin-top-ratio: 0.2;
+}
+</style>
